26e0f1ca0c5807e96323c35d1ff06ff9f02b7d0f
Sho Fujita <sho.foujita@gmail.com>
2025-10-17 18:10:25 +0900

    simple chatbot implementation

--- tools.py
+++ tools.py
@@ -6,11 +6,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 @tool
 def clock():
     """Return current time in UTC stringo format"""
     utc_now = datetime.now(timezone.utc)
     return utc_now.isoformat()
+
 
 search = TavilySearch(max_results=2)
 
